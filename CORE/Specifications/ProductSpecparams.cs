7e89b9762b35885620d4635eb4fe8ec4021a626b
Scrptchore <scriptchore@gmail.com>
2024-01-18 11:52:04 +0100

    Added CorsPolicy

--- CORE/Specifications/ProductSpecparams.cs
+++ CORE/Specifications/ProductSpecparams.cs
@@ -21,5 +21,13 @@ namespace CORE.Specifications
         public int? TypeId {get; set;}
         public string Sort {get; set;}
 
+        private string _search;
+
+        public string Search
+        {
+            get => _search;
+            set => _search = value.ToLower();
+        }
+
     }
 }
