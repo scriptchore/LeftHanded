cf5d547ca03e653dc56204d9817dcd3b1961dae2
Scrptchore <scriptchore@gmail.com>
2024-01-24 01:47:11 +0100

    start lefthanded ui

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Product } from './models/products';
+import { Pagination } from './models/pagination';
 
 @Component({
   selector: 'app-root',
@@ -9,16 +11,17 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
  
   title = 'LeftHanded';
-  products: any[] = [];
+  //products: any[] = [];
+  products: Product[] = [];
 
 
   constructor(private http: HttpClient) {}
 
 
   ngOnInit(): void {
-   this.http.get('https://localhost:5001/api/products?PageSize=50').subscribe({
+   this.http.get<Pagination<Product[]>>('https://localhost:5001/api/products?PageSize=50').subscribe({
     //next: response => console.log(response), //what to do next
-    next: (response: any) => this.products = response.data,
+    next: response => this.products = response.data,
     error: error => console.log(error), // what to do if there is an error
     complete: () => { 
       console.log('request completed');
