11f5e85c24fff20aeceabc46f010118050ec8d6b
Scrptchore <scriptchore@gmail.com>
2024-09-02 14:05:26 +0100

    working basket

--- client/src/app/basket/basket.module.ts
+++ client/src/app/basket/basket.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BasketComponent } from './basket.component';
+import { BasketRoutingModule } from './basket-routing.module';
 
 
 
@@ -9,7 +10,9 @@ import { BasketComponent } from './basket.component';
     BasketComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    BasketRoutingModule
+
   ]
 })
 export class BasketModule { }
