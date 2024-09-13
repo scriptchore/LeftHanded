b52f4d61460194fde28a73e902feed8c22b43cc2
Scrptchore <scriptchore@gmail.com>
2024-09-13 22:12:50 +0100

    login update

--- client/src/app/account/account.module.ts
+++ client/src/app/account/account.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { AccountRoutingModule } from './account-routing.module';
+import { SharedModule } from '../shared/shared.module';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoginComponent,
+    RegisterComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    AccountRoutingModule,
+    SharedModule
   ]
 })
 export class AccountModule { }
